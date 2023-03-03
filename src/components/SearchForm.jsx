a21c8ac5cfb66db254ee0a43458229578f0905a8
JH51 <jonhuang5@gmail.com>
2023-03-03 01:22:05 -0500

    Transitions for delete button

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,27 +1,22 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 
 export default function SearchForm({ addTerm }) {
-  
   const inputRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTerm(inputRef.current.value)
-  }
+    addTerm(inputRef.current.value);
+  };
 
   return (
-    <form
-      id="form"
-      onSubmit={handleSubmit}
-    >
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
-        id="input-text"
         ref={inputRef}
         autoComplete="off"
         placeholder="Domain name or title"
       />
-      <input type="submit" id="buttonSubmit" value="Add" />
+      <input type="submit" value="Add" />
     </form>
   );
 }
--- src/components/TermsList.jsx
+++ src/components/TermsList.jsx
@@ -1,16 +1,41 @@
+import { useRef } from 'react';
+import { RxCross2 } from 'react-icons/rx';
+
 import '../styles/TermsList.css';
+
+function TableEntry({ term }) {
+  const spanRef = useRef();
+
+  const handleMouseEnter = () => {
+    spanRef.current.classList.add('hover-opaque', 'hover-scale');
+  };
+
+  const handleMouseLeave = () => {
+    spanRef.current.classList.remove('hover-opaque', 'hover-scale');
+  };
+
+  return (
+    <tr onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <td className="td-left">{term}</td>
+      <td className="td-right">
+        <span ref={spanRef}>
+          <RxCross2
+            style={{
+              cursor: 'pointer'
+            }}
+          />
+        </span>
+      </td>
+    </tr>
+  );
+}
 
 export default function TermsList({ termsList }) {
   return (
     <table>
       <tbody>
         {termsList.map((term) => {
-          return (
-            <tr key={crypto.randomUUID()}>
-              <td className="td-left">{term}</td>
-              <td className="td-right">X</td>
-            </tr>
-          );
+          return <TableEntry key={crypto.randomUUID()} term={term} />;
         })}
       </tbody>
     </table>
